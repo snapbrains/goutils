8b079ceb16d39d818555bae04ddd32404b88f828
snapbrains <snapbrains@snapbrains.com>
2021-02-16 19:31:08 -0700

    Change var names

--- int64s/int64s.go
+++ int64s/int64s.go
@@ -1,12 +1,11 @@
 package int64s
 
 // Contains returns true if the slice contains the provided value
-func Contains(s []int64, str int64) bool {
-	for _, v := range s {
-		if v == str {
+func Contains(list []int64, x int64) bool {
+	for _, item := range list {
+		if item == x {
 			return true
 		}
 	}
-
 	return false
 }
